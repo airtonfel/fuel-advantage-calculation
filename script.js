dca4d54b911ed935def54e05fd5c7a4c0cd3061c
Airton Feliciano <airtons.f@hotmail.com>
2020-07-19 19:54:08 +0000

    First Commit

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 var etanol, gasolina;
 
-function calcular(){
+function calcular()
+{
     etanol = parseFloat(frmFlex.txtEtanol.value.replace(',' , '.'));
     gasolina = parseFloat(frmFlex.txtGasolina.value.replace(',' , '.'));
       
-    if(etanol < 0.7 * gasolina){
+    if(etanol < 0.7 * gasolina)
+    {
         document.write('Abasteça com: <h3>Etanol!</h3><img src="etanol.png"/><br> O preço está equivalente a 70% ou menos em relação ao preço da gasolina.');
-    } else{
+    }
+    else
+    {
         document.write('Abasteça com: <h3>Gasolina!</h3><img src="gasolina.png"/><br> O preço do Etanol está equivalente a mais de 70% em relação ao preço da gasolina.');
     }
 }
