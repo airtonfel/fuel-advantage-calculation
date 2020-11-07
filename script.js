a82c05b5c17f53851a828d87e7ff882bc117c02d
airtonfel <airtons.f@hotmail.com>
2020-11-07 01:39:02 -0300

    Changes on visual and script

--- script.js
+++ script.js
@@ -4,13 +4,20 @@ function calcular()
 {
     etanol = parseFloat(frmFlex.txtEtanol.value.replace(',' , '.'));
     gasolina = parseFloat(frmFlex.txtGasolina.value.replace(',' , '.'));
-      
-    if(etanol < 0.7 * gasolina)
+
+    if(frmFlex.txtEtanol.value === '' || frmFlex.txtGasolina.value === '')
     {
-        document.write('Abasteça com: <h3>Etanol!</h3><img src="etanol.png"/><br> O preço está equivalente a 70% ou menos em relação ao preço da gasolina.');
+        window.alert('Não podem ser enviados campos em branco! Por favor preencha os campos solicitados.')
     }
-    else
-    {
-        document.write('Abasteça com: <h3>Gasolina!</h3><img src="gasolina.png"/><br> O preço do Etanol está equivalente a mais de 70% em relação ao preço da gasolina.');
-    }
+        else
+        {
+            if(etanol < 0.7 * gasolina)
+            {
+                window.alert('Abasteça com Etanol! O preço está equivalente a 70% ou menos em relação ao preço da gasolina.');
+            }
+            else
+            {
+                window.alert('Abasteça com Gasolina! O preço do Etanol está equivalente a mais de 70% em relação ao preço da gasolina.');
+            }
+        }  
 }
